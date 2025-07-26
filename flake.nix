f52c34359c9438e3fce89862ed34a5f7ae02230c
Anthony Bosio <bosio76@gmail.com>
2025-07-26 09:15:39 -0400

    feat: add Home Manager

--- flake.nix
+++ flake.nix
@@ -1,14 +1,24 @@
 {
   description = "My NixOS configuration";
   inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations = {
       logan = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.abosio = import ./home.nix;
+          }
         ];
       };
     };
---
+++ home.nix
@@ -0,0 +1,35 @@
+
+{ pkgs, ... }:
+
+{
+  home.username = "abosio";
+  home.homeDirectory = "/home/abosio";
+  home.stateVersion = "25.05";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Packages installed for your user
+  home.packages = [
+    phgs.fira-code  
+  ];
+
+  # The starship prompt is a good first package to install.
+  #programs.starship.enable = true;
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "Fira Code";
+      size = 14;
+    };
+    theme = "Solarized Dark";
+  }
+
+  # Some services
+  services.gpg-agent = {
+    enable = true;
+    defaultCacheTtl = 1800;
+    enableSshSupport = true;
+  };
+
+}
