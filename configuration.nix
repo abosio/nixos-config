e258273697ffb68d645ac99ecda40fecb805cab4
Anthony Bosio <bosio76@gmail.com>
2025-07-29 22:32:39 -0400

    fix: hostname in active config copy

--- configuration.nix
+++ configuration.nix
@@ -23,7 +23,7 @@
   '';
   # END ABOSIO  
 
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "logan"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
