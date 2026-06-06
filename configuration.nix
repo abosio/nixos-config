57e0a4844a5865c0aa2ef00460b378b48c858884
Anthony Bosio <bosio76@gmail.com>
2026-06-06 15:04:39 -0400

    refactor: extract printing.nix system module

--- configuration.nix
+++ configuration.nix
@@ -11,6 +11,7 @@
       ./modules/nixos/common.nix
       ./modules/nixos/fonts.nix
       ./modules/nixos/desktop-gnome.nix
+      ./modules/nixos/printing.nix
     ];
 
   # Bootloader.
@@ -30,31 +31,6 @@
   # Enable hardware acceleration for AMD GPUs
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.printing.drivers = with pkgs; [
-    gutenprint        # High-quality drivers for Canon, Epson, Lexmark, Sony, Olympus
-    hplip             # HP printers
-    brlaser           # Brother laser printers
-    brgenml1lpr       # Brother generic driver
-    brgenml1cupswrapper
-  ];
-  # Enable printer discovery via Avahi
-  services.avahi.publish.enable = true;
-  services.avahi.publish.userServices = true;
-
-  # Configure Brother HL-2170W printer
-  hardware.printers.ensurePrinters = [{
-    name = "Brother_HL-2170W";
-    location = "Home";
-    deviceUri = "ipp://BRN001BA92DE10D.local/ipp/port1";
-    model = "drv:///brlaser.drv/br2140.ppd";
-    description = "Brother HL-2170W";
-    ppdOptions = {
-      PageSize = "Letter";
-    };
-  }];
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
