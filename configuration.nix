e6dd711bb19c8f7467e86c2802986d4a83f1ff8f
Anthony Bosio <bosio76@gmail.com>
2026-06-06 15:01:22 -0400

    refactor: extract common.nix system module

--- configuration.nix
+++ configuration.nix
@@ -5,21 +5,15 @@
 { inputs, config, pkgs, ... }:
 
 {
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./modules/nixos/common.nix
     ];
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  system.autoUpgrade.enable = true;
-  system.autoUpgrade.allowReboot = false;
-  networking.extraHosts = ''
-    167.71.175.50   git.abosio.com
-  '';
 
   networking.hostName = "logan"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -27,31 +21,6 @@
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;  # Enable mDNS resolution for .local domains
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/New_York";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
@@ -98,27 +67,10 @@
     };
   }];
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
-
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  programs.zsh.enable = true;
+  # Define a user account. Don’t forget to set a password with ‘passwd’.
   users.users.abosio = {
     isNormalUser = true;
     description = "Anthony Bosio";
@@ -128,12 +80,6 @@
     #  thunderbird
     ];
   };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "electron-39.8.10" # required by logseq, pending upstream update
-  ];
 
   fonts = {
     packages = with pkgs; [
@@ -161,22 +107,6 @@
     # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "abosio" ];
   };
-  # enable non-nix binaries
-  programs.nix-ld.enable = true;
-
-  # Add Caddy internal CA certificate for *.abosio-cloud.com
-  security.pki.certificateFiles = [
-    "${inputs.nixos-secrets}/caddy-root-ca.crt"
-  ];
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-    pkgs.vim
-  ];
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -186,9 +116,6 @@
   # };
 
   # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
 
   services.tailscale.enable = true;
 
---
+++ modules/nixos/common.nix
@@ -0,0 +1,63 @@
+# modules/nixos/common.nix
+# Base settings every machine in the fleet gets.
+{ inputs, pkgs, ... }:
+
+{
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.permittedInsecurePackages = [
+    "electron-39.8.10" # required by logseq, pending upstream update
+  ];
+
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = false;
+
+  networking.networkmanager.enable = true;
+  networking.extraHosts = ''
+    167.71.175.50   git.abosio.com
+  '';
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true; # Enable mDNS resolution for .local domains
+  };
+
+  services.openssh.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  programs.zsh.enable = true;
+  programs.nix-ld.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    vim
+  ];
+
+  # Add Caddy internal CA certificate for *.abosio-cloud.com
+  security.pki.certificateFiles = [
+    "${inputs.nixos-secrets}/caddy-root-ca.crt"
+  ];
+}
